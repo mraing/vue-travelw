92f9ed3311db62f35e622cd4753fe14d0e63d9ae
绪锋 <910408228@qq.com>
2019-06-30 23:47:06 +0800

    新增首页轮播图w
    引入 Vue-Awesome-Swiper 轮播图第三方库

--- src/main.js
+++ src/main.js
@@ -7,11 +7,20 @@ import router from './router'
 // 引入第三方库（解决移动端点击延迟）
 import fastClick from 'fastClick'
 
+// 引入轮播图第三方库
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// 引入轮播图样式
+import 'swiper/dist/css/swiper.css'
+
 // 引入基本样式
 import 'styles/reset.css'
 import 'styles/border.css'
 
 Vue.config.productionTip = false
+
+// 使用轮播图
+Vue.use(VueAwesomeSwiper)
 
 // 引入第三方库（解决移动端300ms点击延迟）
 fastClick.attach(document.body)
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" class="wrapper-touch">
+    <!-- slides -->
+    <swiper-slide v-for="items in swoperList" :key="items.id"><img class="swiper-img" :src="items.imgUrl" :alt="items.imgAlt" /></swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swoperList: [ {
+        id: '0001',
+        imgUrl: 'https://dwz.cn/4uPKTU30',
+        imgAlt: '张家界'
+      }, {
+        id: '0002',
+        imgUrl: 'https://dwz.cn/gKlMptiH',
+        imgAlt: '天门山'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wrapper
+  position relative
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 26.66%
+  background #eee
+  .swiper-img
+    width 100%
+</style>
