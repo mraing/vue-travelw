ae0efa0b58d80c6ff0848c6f0b03c4d87a34728c
绪锋 <910408228@qq.com>
2019-07-01 23:59:26 +0800

    Add iconImg
     添加图标文件

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        <div>i'm icons</div>
+    <div class="icons">
+        <div class="icon" v-for="items in iconsList" :key="items.id">
+            <img :src="items.imgUrl" :alt="items.title">
+        </div>
     </div>
 </template>
 
 <script> 
 export default {
-    
+    data () {
+        return {
+            iconsList:[ {
+                id: '0001',
+                title: '热门',
+                imgUrl: 'https://dwz.cn/dHCQaqIi'
+            } ]
+        }
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.icons
+    overflow hidden
+    height 0
+    padding-bottom 50%
+    background green
+    .icon
+        float left
+        width 25%
+        padding-bottom 25%
 </style>
