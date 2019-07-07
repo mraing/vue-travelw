6786c5f9826d5ea4d530933cb3a83470deaa999f
绪锋 <910408228@qq.com>
2019-07-07 21:37:49 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,10 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont">&#xe601;</span>
-    </div>
+    <router-link to="/city" class="header-right">
+        {{this.city}}
+        <span class="iconfont">&#xe601;</span>
+    </router-link>
   </div>
 </template>
 
@@ -30,31 +30,33 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .header
-    display: flex
-    line-height: .86rem
-    background: $bgcolor
-    color: #fff
+    display flex
+    line-height headerHeight
+    background $bgcolor
+    color #fff
     .header-left
-      width: .64rem
-      float: left
+      width .64rem
+      float left
       .back-icon
-        text-align: center
+        text-align center
     .header-input
-      flex: 1
-      height : .64rem
-      color : #666
-      line-height : .64rem
-      margin-top : .12rem
+      flex 1
+      height .64rem
+      color #666
+      line-height .64rem
+      margin-top .12rem
       margin-left .2rem
-      padding-left :.2rem
-      background :#fff
-      border-radius : .1rem
+      padding-left .2rem
+      background #fff
+      border-radius .1rem
     .header-right
-      box-sizing : border-box
-      padding-right : .1rem
-      width: 1.24rem
-      float: right
-      text-align :right
+      display block
+      box-sizing border-box
+      padding-right .1rem
+      width 1.24rem
+      float right
+      text-align right
+      color #fff
       .arrow-icon
-        margin-left : -.04rem
+        margin-left  -.04rem
 </style>
